754092a0a14f9046c22658d0d27529b4ed36a3a5
Avinash <avinash.rastogi21@outlook.com>
2019-05-18 10:19:04 +0530

    Adding Console Application ArraySum - HackerRank Test

---
+++ ArraySum/ArraySum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySum
+{
+    class ArraySum
+    {
+        static void Main(string[] args)
+        {
+
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("Path"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int result = simpleArraySum(ar);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+           
+        }
+
+
+        static int simpleArraySum(int[] ar)
+        {
+            /*
+             * Write your code here.
+             */
+            int result = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                result += ar[i];
+            }
+
+            return result;
+        }
+    }
+}
